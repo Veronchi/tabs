9331ace018090b5b68ad9032989cf029edce1918
Veronchi <zhigunova1995@gmail.com>
2021-12-10 17:43:12 +0300

    implement fetching data and loading

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [tabData, setTabData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+
+      const response = await fetch(url);
+      const responseData = await response.json()
+
+      setTabData(responseData);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    console.log(111)
+    return (
+      <div className='loading'>
+        <h1>loading...</h1>
+      </div>
+    )
+  }
+
+  return(1)
+
 }
 
 export default App
