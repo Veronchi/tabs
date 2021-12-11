8d1140b4a7ebfd2a9175870d99e1d673ab7343ef
Veronchi <zhigunova1995@gmail.com>
2021-12-11 18:55:47 +0300

    implement section with content

--- src/App.js
+++ src/App.js
@@ -5,23 +5,22 @@ const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
   const [tabData, setTabData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [activeTab, setActiveTab] = useState({});
 
   useEffect(() => {
     async function fetchData() {
-      setIsLoading(true);
-
       const response = await fetch(url);
       const responseData = await response.json()
 
       setTabData(responseData);
+      setActiveTab(responseData[0]);
       setIsLoading(false);
     }
     fetchData();
   }, []);
 
   if (isLoading) {
-    console.log(111)
     return (
       <div className='loading'>
         <h1>loading...</h1>
@@ -29,7 +28,36 @@ function App() {
     )
   }
 
-  return(1)
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          <button className='job-btn'>{activeTab.company}</button>
+          <button className='job-btn'>{activeTab.company}</button>
+          <button className='job-btn'>{activeTab.company}</button>
+        </div>
+        <article key={activeTab.id} className='job-info'>
+          <h3>{activeTab.title}</h3>
+          <h4>{activeTab.company}</h4>
+          <p className='job-date'>{activeTab.dates}</p>
+
+          {activeTab.duties.map((duty, id) => {
+            return (
+              <div key={id} className='job-desc'>
+                <FaAngleDoubleRight />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <button type='button' className='btn'>more info</button>
+    </section>
+  );
 
 }
 
